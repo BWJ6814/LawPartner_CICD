621fdc7b1dbbcf59925141c3926fee2d3379e447
amino6413-lab <amino6413@gmail.com>
2026-02-09 12:06:12 +0900

    26-02-09

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';  <-- 공용
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './ky/App'; // 김용
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
+reportWebVitals();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
